e2d01c62bd2c6fa5c8d609671234cf49b2cbbeb8
sakibsarkar <sakibsarkar707@gmail.com>
2024-06-09 12:50:51 +0600

    tash

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -2,16 +2,38 @@
 import Input from "@/components/steps/Input";
 import Card from "@/components/steps/card";
 import { Form, Formik } from "formik";
+import { useState } from "react";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 const initialValues = {
   test: "",
 };
-const page = () => {
+const Page = () => {
+  const [showPass, setShowPass] = useState(true);
+
   return (
     <Card heading="Sign in to your account">
       <Formik initialValues={initialValues} onSubmit={(e) => console.log(e)}>
         {({ errors, touched }) => (
           <Form className="w-full mx-auto flex flex-col gap-[20px]">
-            <Input name="emailOrNumber" title="Email/Number"/>
+            <Input
+              name="emailOrNumber"
+              title="Email/Number"
+              placeholder="Type your mail"
+            />
+            <div className="w-full relative">
+              <Input
+                name="password"
+                title="Password"
+                type={showPass ? "text" : "password"}
+                placeholder="Type your Password"
+              />
+              <button
+                onClick={() => setShowPass(!showPass)}
+                className="text-[#718096] absolute right-[20px] top-[51px]"
+              >
+                {showPass ? <FiEyeOff /> : <FiEye />}
+              </button>
+            </div>
             <button
               type="submit"
               className="bg-primary text-white px-5 py-3 mb-5 rounded-md disabled:bg-gray-400"
@@ -25,4 +47,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
