e327b778bd1feb16e03711ed2fa2fa13e9a9a8f5
sakibsarkar <sakibsarkar707@gmail.com>
2024-06-25 17:05:59 +0600

    button style update

--- src/components/steps/StepTwo.tsx
+++ src/components/steps/StepTwo.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import { setCustomerDetailTitle } from "@/redux/features/customer-detail/customerDetailSlice";
+import { useAppDispatch } from "@/redux/hook";
 import { useState } from "react";
 import StepBody from "../shared/StepBody";
-import { useAppDispatch } from "@/redux/hook";
-import { setCustomerDetailTitle } from "@/redux/features/customer-detail/customerDetailSlice";
 
 const StepTwo = () => {
   const [currentItem, setCurrentItem] = useState<string>("");
@@ -40,13 +40,12 @@ const StepTwo = () => {
                 setCurrentItem(item.value);
                 dispatch(setCustomerDetailTitle({ title: item?.value }));
               }}
-              className={`outline hover:outline-slate-800 border-2 hover:border-white w-full py-[12px] px-[24px] bg-slate-950 text-white rounded-[12px] ${
-                item.value === currentItem
-                  ? "outline outline-slate-800 border-2 border-white"
-                  : ""
-              }`}
+              className={`relative inline-flex overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 `}
             >
-              {item.value}
+              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+              <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-3 text-sm font-medium text-white backdrop-blur-3xl">
+                {item.value}
+              </span>
             </button>
           ))}
         </div>
