4372634305a66a08835b9ac6565820c086b056bf
sakibsarkar <sakibsarkar707@gmail.com>
2024-07-04 17:06:50 +0600

    footer content added in steps flow

--- src/components/ui/gradient-container.tsx
+++ src/components/ui/gradient-container.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 import React, { ReactNode } from "react";
 
 const GradientContainer: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const footer = [
+    {
+      href: "#",
+      text: "Privacy Policy",
+    },
+    {
+      href: "#",
+      text: "Privacy Policy",
+    },
+    {
+      href: "#",
+      text: "License",
+    },
+  ];
+
   return (
-    <div className="w-full min-h-[100vh] relative overflow-hidden flex  items-center justify-center">
+    <div className="w-full min-h-[100vh] relative overflow-hidden flex  items-center justify-around flex-col">
       <Image
         src={"/img/auraFrameL.png"}
         width={958}
@@ -20,6 +36,16 @@ const GradientContainer: React.FC<{ children: ReactNode }> = ({ children }) => {
       />
       <div className=" py-[20px] lg:py-0 relative z-50 w-full px-[15px] lg:px-0">
         {children}
+      </div>
+      <div className="flex justify-center md:justify-between flex-wrap gap-[15px] items-center w-[90%] lg:w-[900px]">
+        <h6 className="text-[#718096]">@ 2024, Made with ❤️ by CreditUP</h6>
+        <div className="center gap-[10px] text-[#718096]">
+          {footer.map(({ href, text }, i) => (
+            <Link href={href} key={i + "footer link"}>
+              {text}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
