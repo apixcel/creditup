e3ea105fdeedbfd3f91816a53a72bd230bc9ab40
sakibsarkar <sakibsarkar707@gmail.com>
2024-07-10 14:55:09 +0600

    review profile update

--- src/app/(page)/page.tsx
+++ src/app/(page)/page.tsx
@@ -3,6 +3,7 @@ import { HeroBanner } from "@/components/banner/HereBanner";
 import { CardHoverEffect } from "@/components/card/CardHoverEffect";
 import { Testimonial } from "@/components/carosol/Testimonial";
 import NumberCounter from "@/components/counter/NumberCounter";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -70,12 +71,16 @@ export default function Home() {
           </div>
           <div className="w-full mt-5 flex flex-col items-center">
             <Testimonial />
-            <button className="mt-5 relative inline-flex overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <Link
+              href={"https://reviewthis.biz/Creditup-uk"}
+              target="_blank"
+              className="mt-5 relative inline-flex overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+            >
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
               <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-3 text-sm font-medium text-white backdrop-blur-3xl">
                 Leave Your Review
               </span>
-            </button>
+            </Link>
           </div>
         </div>
       </section>
